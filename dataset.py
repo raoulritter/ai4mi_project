986c29df4934261b2241ab4cf547c33344241e2d
Didier Merk <didier.merk@gmail.com>
2024-10-13 17:29:37 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -25,40 +25,105 @@
 from pathlib import Path
 from typing import Callable, Union
 
-from torch import Tensor
 from PIL import Image
+from torch import Tensor
 from torch.utils.data import Dataset
 
 
-def make_dataset(root, subset) -> list[tuple[Path, Path]]:
-    assert subset in ['train', 'val', 'test']
+# def make_dataset(root, subset) -> list[tuple[Path, Path]]:
+#     assert subset in ["train", "val", "test"]
+
+#     root = Path(root)
+
+#     img_path = root / subset / "img"
+#     full_path = root / subset / "gt"
+
+#     images = sorted(img_path.glob("*.png"))
+#     full_labels = sorted(full_path.glob("*.png"))
+
+#     # our new implementation for sorting images ours above was first
+#     # Sort images and labels by patient ID and slice index
+#     images = sorted(
+#         images, key=lambda x: (x.stem.split("_")[1], int(x.stem.split("_")[2]))
+#     )
+#     full_labels = sorted(
+#         full_labels, key=lambda x: (x.stem.split("_")[1], int(x.stem.split("_")[2]))
+#     )
+
+#     return list(zip(images, full_labels))
+
+# Make dataset with augmentation flag:
+
+def make_dataset(root, subset, augment=False) -> list[tuple[Path, Path]]:
+    assert subset in ["train", "val", "test"]
 
     root = Path(root)
 
-    img_path = root / subset / 'img'
-    full_path = root / subset / 'gt'
+    img_dir = root / subset / "img"
+    gt_dir = root / subset / "gt"
 
-    images = sorted(img_path.glob("*.png"))
-    full_labels = sorted(full_path.glob("*.png"))
+    img_files = sorted(img_dir.glob("*.png"))
+    gt_files = sorted(gt_dir.glob("*.png"))
 
-    # our new implementation for sorting images ours above was first
-    # Sort images and labels by patient ID and slice index
-    images = sorted(images, key=lambda x: (x.stem.split('_')[1], int(x.stem.split('_')[2])))
-    full_labels = sorted(full_labels, key=lambda x: (x.stem.split('_')[1], int(x.stem.split('_')[2])))
+    # Ensure that image and ground truth files match
+    assert len(img_files) == len(gt_files), "Number of images and ground truths do not match"
 
-    return list(zip(images, full_labels))
+    img_filenames = [img.name for img in img_files]
+    gt_filenames = [gt.name for gt in gt_files]
+
+    assert img_filenames == gt_filenames, "Image and ground truth filenames do not match"
+
+    # Create list of tuples for original data
+    data = list(zip(img_files, gt_files))
+
+    if augment and subset == 'train':
+        # Include augmented data
+        aug_img_dir = root / subset / "img_aug"
+        aug_gt_dir = root / subset / "gt_aug"
+
+        aug_img_files = sorted(aug_img_dir.glob("*.png"))
+        aug_gt_files = sorted(aug_gt_dir.glob("*.png"))
+
+        # Ensure that augmented image and ground truth files match
+        assert len(aug_img_files) == len(aug_gt_files), "Number of augmented images and ground truths do not match"
+
+        aug_img_filenames = [img.name for img in aug_img_files]
+        aug_gt_filenames = [gt.name for gt in aug_gt_files]
+
+        assert aug_img_filenames == aug_gt_filenames, "Augmented image and ground truth filenames do not match"
+
+        # Create list of tuples for augmented data
+        aug_data = list(zip(aug_img_files, aug_gt_files))
+
+        # Combine original and augmented data
+        data += aug_data
+
+    # Now, sort the combined data
+    data = sorted(
+        data, key=lambda x: (x[0].stem.split("_")[1], int(x[0].stem.split("_")[2]))
+    )
+
+    return data
 
 
 class SliceDataset(Dataset):
-    def __init__(self, subset, root_dir, img_transform=None,
-                 gt_transform=None, augment=False, equalize=False, debug=False):
+    def __init__(
+        self,
+        subset,
+        root_dir,
+        img_transform=None,
+        gt_transform=None,
+        augment=False,
+        equalize=False,
+        debug=False,
+    ):
         self.root_dir: str = root_dir
         self.img_transform: Callable = img_transform
         self.gt_transform: Callable = gt_transform
         self.augmentation: bool = augment
         self.equalize: bool = equalize
 
-        self.files = make_dataset(root_dir, subset)
+        self.files = make_dataset(root_dir, subset, augment=self.augmentation)
         if debug:
             self.files = self.files[:10]
 
@@ -102,7 +167,7 @@ class SliceDataset(Dataset):
 
         # Extract patient ID and slice index
         stem = img_path.stem  # 'Patient_03_0000'
-        parts = stem.split('_')
+        parts = stem.split("_")
         patient_id = f"{parts[0]}_{parts[1]}"  # 'Patient_03'
         slice_idx = int(parts[2])  # '0000'
 
@@ -111,5 +176,5 @@ class SliceDataset(Dataset):
             "gts": gt,
             "stems": stem,
             "patient_id": patient_id,
-            "slice_idx": slice_idx
+            "slice_idx": slice_idx,
         }
